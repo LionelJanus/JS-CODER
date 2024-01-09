528c52a86d982e827330a27934ade3b2d53c495e
Lionel <lj.lionel@gmail.com>
2024-01-09 20:34:00 -0300

    first commit

---
+++ js/arrayproductos.js
@@ -0,0 +1,65 @@
+const productosBaseDeDatos = [
+  {
+    id: 1,
+    name: "Golden",
+    description: "Notas bla bla",
+    precio: "",
+  },
+  {
+    id: 2,
+    name: "PaleAle",
+    description:
+    " rica en lúpulo.",
+    precio: "",
+  },
+  {
+    id: 3,
+    name: "Irish Red",
+    description: "",
+    precio: "",
+  },
+  {
+    id: 4,
+    name: "IPA",
+    description: "Notas bla bla",
+    precio: "",
+  },
+  {
+    id: 5,
+    name: "APA",
+    description: "Notas bla bla",
+    precio: "",
+  },
+  {
+    id: 6,
+    name: "Stout",
+    description: "a veces llamada cerveza negra en España.  ",
+    precio: "",
+  },
+  {
+    id: 7,
+    name: "Brown",
+    description: "Notas bla bla",
+    precio: "",
+  },
+  {
+    id: 8,
+    name: "Lager",
+    description: "Notas bla bla",
+    precio: "",
+  },
+  {
+    id: 9,
+    name: "Kolsh",
+    description: "Notas bla bla",
+    precio: "",
+  },
+  {
+    id: 10,
+    name: "Porter",
+    description: "son para muchos un tipo más de Stout, pero algo menos oscuras .",
+    precio: "500",
+  },
+];
+
+
---
+++ js/main.js
@@ -0,0 +1,91 @@
+const container = document.getElementById("container");
+productosBaseDeDatos.forEach((el, idx) => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const nombreProducto = document.createElement("h4")
+    nombreProducto.innerText = el.name;
+
+    const imgProducto = document.createElement("img");
+    imgProducto.src = el.img;
+
+    const description = document.createElement("h5")
+    description.innerText = el.description;
+    
+    const precio = document.createElement("h4")
+    precio.innerText = el.precio;
+    
+    
+    const btnInfo = document.createElement("button");
+    btnInfo.innerText = "Agregar";
+    btnInfo.onclick = function() {
+        addToCart(el.id);
+    };
+    
+    
+
+    
+    card.appendChild(nombreProducto);
+    card.appendChild(imgProducto);
+    card.appendChild(description)
+    card.appendChild(precio);
+    card.appendChild(btnInfo);
+    
+   
+    container.appendChild(card);
+})
+
+// Declaro la funcion que recibe como parametro el id de producto
+function addToCart(id){
+    // Obtengo el carrito, compruebo que exista, sino existe, lo declaro como un array vacio
+    let cart = JSON.parse(localStorage.getItem('carrito')) ? JSON.parse(localStorage.getItem('carrito')) : [];
+
+    
+    // Llamo a los producto
+    const products = productosBaseDeDatos // Este seria tu array;
+
+    // Busco en los productos por el id que me llega de parametro
+    let productToFind = products.find(product => parseInt(product.id) === parseInt(id));
+
+    // Si el producto existe
+    if(productToFind){
+
+        // Busco si ese mismo producto ya fue agregado al carrito anteriormente por su id
+        let productInCart = cart.find(product => product.id === id);
+
+        // Si ya existe en el carrito
+        if(productInCart){
+            // Le aumento la cantidad
+            productInCart.cantidad++;
+        }
+        // Si no existe en mi carrito, lo agrego
+        else {
+
+            cart.push({
+                id: productToFind.id,
+                name: productToFind.name,
+                description: productToFind.description,
+                precio: productToFind.precio,
+                
+                cantidad: 1
+            })
+        }
+
+        // Una vez realizado todo, guardo el carrito en localStorage
+        localStorage.setItem('carrito', JSON.stringify(cart));
+    }
+    function sumaUnidades(carrito) {
+        let totalFinal
+        totalFinal=0
+        for (localStorage of carrito) {
+          totalFinal = totalFinal + (productToFind.precio * productInCart.cantidad)
+        }
+    
+        console.log(totalFinal)
+    
+    }
+}
+
+
+
+
